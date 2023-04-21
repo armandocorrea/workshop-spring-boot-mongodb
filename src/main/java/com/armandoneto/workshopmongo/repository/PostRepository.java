1328eabac116909a057cee1e7fa55e2c686153bb
armando.neto <armando.neto@senior.com.br>
2023-04-21 19:30:22 -0300

    Consulta simples com query methods

--- src/main/java/com/armandoneto/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/armandoneto/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.armandoneto.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.armandoneto.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String> {	
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
